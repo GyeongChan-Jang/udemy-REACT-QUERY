5b034eeecdc2079dfcf08c9faec893cdd98e4743
chan <cham9994@gmail.com>
2022-09-09 02:55:34 +0900

    #1 리액트 쿼리 기본 개념 - useQuery, staleTime, cacheTime

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,31 +1,27 @@
-import { useState } from "react";
-
-import { PostDetail } from "./PostDetail";
-const maxPostPage = 10;
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { PostDetail } from './PostDetail'
+const maxPostPage = 10
 
 async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
-  return response.json();
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0')
+  return response.json()
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0)
+  const [selectedPost, setSelectedPost] = useState(null)
 
   // replace with useQuery
-  const data = [];
+  const { data, isError, isLoading } = useQuery('posts', fetchPosts, { staleTime: 2000 })
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>something went wrong</div>
 
   return (
     <>
       <ul>
         {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
@@ -42,5 +38,5 @@ export function Posts() {
       <hr />
       {selectedPost && <PostDetail post={selectedPost} />}
     </>
-  );
+  )
 }
